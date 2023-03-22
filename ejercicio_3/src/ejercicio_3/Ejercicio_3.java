3f9bf447c84c322387bd461252151dad319a8bce
Aaron Vigil <aaronvigil96@gmail.com>
2023-03-21 22:39:17 -0300

    exercise 3 finished

--- ejercicio_3/src/ejercicio_3/Ejercicio_3.java
+++ ejercicio_3/src/ejercicio_3/Ejercicio_3.java
@@ -3,18 +3,47 @@
     Después haremos otra función o procedimiento que imprima el vector.
 */
 package ejercicio_3;
+import java.util.Scanner;
 
 public class Ejercicio_3 {
 
     public static void main(String[] args) {
-        int[] vector = new int[5];
-        for (int i = 0; i < vector.length; i++) {
-            if(i <= 1){
-                vector[i] = 1;
-            }else{
-                vector[i] = vector[i - 1] + vector[i - 2];
+        Scanner entrada = new Scanner(System.in);
+        boolean bucle = true;
+        int opcion, longitud;
+        System.out.println("Ingresa la longitud del vector");
+        longitud = entrada.nextInt();
+        int[] vector = new int [longitud];
+        do{
+            System.out.println("¿Que desea hacer?");
+            System.out.println("1)- Llenar vector");
+            System.out.println("2)- Mostrar vector");
+            System.out.println("0)- Salir");
+            opcion = entrada.nextInt();
+            switch(opcion){
+                case 1: {
+                    llenarVector(vector);
+                    break;
+                }
+                case 2: {
+                    mostrarVector(vector);
+                    break;
+                }
+                case 0: {
+                    bucle = false;
+                    break;
+                }
             }
-            System.out.println(vector[i]);
+        }while(bucle);
+    }
+    public static void llenarVector(int[] vector){
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (int)(Math.random() * 10);
+        }
+    }
+    public static void mostrarVector(int[] vector){
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print(vector[i] + " ");
         }
     }
     
